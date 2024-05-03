55f6d16ff898e34cc3699d04a40136f31daf110f
Alejandra Flaño <aflanog@latiaencanto.cl>
2024-05-02 21:46:00 -0400

    se comienza a configurar el servidor de correo y un correo basico

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const nodemailer = require('nodemailer');
+require('dotenv').config();
 
 //configuracion de correo usando gmail
 let transporter = nodemailer.createTransport({
@@ -8,3 +9,11 @@ let transporter = nodemailer.createTransport({
         pass: process.env.USER_PASSWORD
     }
 });
+
+let mailOptions = {
+    from: process.env.USER_EMAIL, //Quien lo envia
+    to: process.env.USER_EMAIL, // Quien lo recibe
+    subject: 'Prueba de correo', //  Asunto
+    html: '<h1>Prueba de correo</h1>' // Cuerpo del correo
+   // text: 'Prueba de correo' // Cuerpo del correo
+};
